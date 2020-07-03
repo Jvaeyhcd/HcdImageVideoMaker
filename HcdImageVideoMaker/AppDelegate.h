4a7be6c7914f51e879bdab0b2a99c838d38b94c2
Jvaeyhcd <chedahuang@icloud.com>
2020-07-03 17:42:06 +0800

    first commit

---
+++ HcdImageVideoMaker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HcdImageVideoMaker
+//
+//  Created by Salvador on 2020/7/3.
+//  Copyright © 2020 Salvador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HcdImageVideoMaker/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HcdImageVideoMaker
+//
+//  Created by Salvador on 2020/7/3.
+//  Copyright © 2020 Salvador. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
